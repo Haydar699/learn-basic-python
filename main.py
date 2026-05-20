d0b2b2e1ad984bfc918c8d41b33b880df7d59bbc
Muhammad Haydar Fatahillah <muhammad.haydar.fatahillah@gmail.com>
2026-05-20 10:57:29 +0700

    add a new student

--- main.py
+++ main.py
@@ -7,7 +7,10 @@ class Mahasiswa:
 mhs001 = Mahasiswa("seno","TI","saintek")
 mhs002 = Mahasiswa("Budi","HI","Humaniora")
 mhs003 = Mahasiswa("Karno","EI","Syariah")
+mhs004 = Mahasiswa("Jono","AFI","Ushuluddin")
 
 print(f"nama: {mhs001.nama}, prodi: {mhs001.prodi}, fakultas: {mhs001.fakultas}")
 print(f"nama: {mhs002.nama}, prodi: {mhs002.prodi}, fakultas: {mhs002.fakultas}")
 print(f"nama: {mhs003.nama}, prodi: {mhs003.prodi}, fakultas: {mhs003.fakultas}")
+print(f"nama: {mhs004.nama}, prodi: {mhs004.prodi}, fakultas: {mhs004.fakultas}")
+
